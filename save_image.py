a049b120b75966c2d529f3abb63951c099b07881
sufyan saori <blackmanga80@gmail.com>
2019-04-22 08:08:09 +0700

    add folder to save image

--- save_image.py
+++ save_image.py
@@ -6,11 +6,21 @@
 '''
 
 import cv2
+import os
 
 id_cam = 0
 cap = cv2.VideoCapture(id_cam)
 index = 0
-name = input('masukkan nama orang : ')
+name = input('masukkan npm : ')
+path = "foto/"+name
+# check jika folder sudah ada
+if os.path.isdir(path) :
+    y = os.listdir(path)
+    if (y) :
+        index = sorted([int(os.path.splitext(x)[0]) for x in y])[-1]+1
+        print(index)
+else:
+    os.mkdir("foto/"+name)
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -21,8 +31,10 @@ while cap.isOpened():
     
     # save image
     if key == ord("c"):
-        namefile = name+str(index)+'.png'
+        print('take picture')
+        namefile = os.path.join (path+"/"+str(index)+'.png')
         cv2.imwrite(namefile, frame)
+        index += 1
     if key == ord("q"):
         cv2.destroyAllWindows()
         break
